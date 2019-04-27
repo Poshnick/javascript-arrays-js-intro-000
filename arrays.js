3fac762ef2822ee8e432dda413e349e3e4b295bc
Nick Cooper <nickcooper84@gmail.com>
2019-04-27 18:23:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return ["wispa", ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  chocolateBars = ["wispa", ...chocolateBars];
+}
