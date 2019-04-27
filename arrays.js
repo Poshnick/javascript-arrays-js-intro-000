e337deb0f20e5db726ceda332cc96ed087f0838b
Nick Cooper <nickcooper84@gmail.com>
2019-04-27 18:12:27 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars.unshift("aero") {
+
+}
+
+function destructivelyAddElementToBeginningOfArray("aero", ...chocolateBars) {
+
+}
